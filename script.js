bdaa62dbd4254164ae52658de943bae9b7a74cd2
Richard Kinyua <59767249+vrichie@users.noreply.github.com>
2026-02-04 18:57:16 +0300

    intro to variables

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 // This is JavaScript!
+// java system.out.println("Hello world")
+//  c printf("Hello world")
 console.log("Hello, World!");
-console.log("Welcome to JavaScript Fundamentals");
-console.log("Instructor: Richard Kinyua");
+
+
+let age = 21.5; // this value can change
+var name = false; // old way
+const isStudent = false; //constant, it  won't change
+const pi=3.141;
+console.log(age)
+age = "jane";
+const student ={name:"Jane",isStudent:false,age:21};
+const marks = [23,45,235,"jane"]
+console.log(student.name)
+console.log(marks)
